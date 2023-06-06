517137b0944610774da2865d2a6973f5b4fd693d
Robert Alvarez <alvarezrobert150@gmail.com>
2023-06-06 08:50:10 -0400

    video 20

--- apps/products/api/views/product_view.py
+++ apps/products/api/views/product_view.py
@@ -1,5 +1,21 @@
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+
 from apps.products.api.serializers.product_serializers import ProductSerializer
 from apps.base.views import BaseGenericListAPIView
 
 class ProductListAPIView(BaseGenericListAPIView):
     serializer_class = ProductSerializer
+    
+class ProductCreateAPIView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+    
+    
+    def post(self, request):
+        serializer = self.get_serializer()
+        serializer = serializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Producto creado correctamente'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
