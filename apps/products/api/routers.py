defb7d7ead3435fd860aac3eea978c8841ebabbf
Robert Alvarez <alvarezrobert150@gmail.com>
2023-06-19 09:25:25 -0400

    video 26

--- apps/products/api/routers.py
+++ apps/products/api/routers.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from .views.product_viewsets import ProductViewSet
+from .views.general_view import *
 
 routers = DefaultRouter()
 routers.register('products', ProductViewSet, basename= 'products')
+routers.register('measure-unit', MeasureUnitViewset, basename= 'measure')
+routers.register('indicators', IndicatorListViewset, basename= 'indicators')
+routers.register('category-products', CategoryProductViewset, basename= 'category')
 
 urlpatterns = routers.urls
 
--- apps/products/api/views/general_view.py
+++ apps/products/api/views/general_view.py
@@ -1,11 +1,24 @@
-from apps.base.views import BaseGenericListAPIView
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from apps.products.models import MeasureUnit
 from apps.products.api.serializers.general_serializers import MeasureUnitSerializer, CategoryProductSerializer, IndicatorSerializer
 
-class MeasureUnitListAPIView(BaseGenericListAPIView):
+class MeasureUnitViewset(viewsets.GenericViewSet):
     serializer_class = MeasureUnitSerializer
+    model = MeasureUnit
     
-class CategoryProductListAPIView(BaseGenericListAPIView):
+    def get_queryset(self):
+        return super().get_serializer().Meta.model.objects.filter(state = True)
+    
+    def list(self, request):
+        data = self.get_queryset()
+        data = self.get_serializer(data, many = True)
+        return Response(data.data)
+    
+    
+class CategoryProductViewset(viewsets.GenericViewSet):
     serializer_class = CategoryProductSerializer
     
-class IndicatorListAPIView(BaseGenericListAPIView):
+class IndicatorListViewset(viewsets.GenericViewSet):
     serializer_class = IndicatorSerializer
